5c668440175f8f9943d1e4e0c8f3dffa7aba1ef8
VINAYAK KUMAR JHA <VINAYAKKUMARJHA2000@GMAIL.COM>
2024-04-14 17:35:27 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name="gen AI Project",
+       version="0.0.0",
+      author="mevinayakjha",
+      author_email="vinayakkumarjha2000@gmail.com",
+      packages=find_packages()
+)
